97ab00740d9bf945938f8fbc5827e03b8246acb9
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-10-15 14:29:37 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,15 +1,20 @@
-// src/components/NavBar/index.js
 import React from 'react'
 import './index.css'
 
-const NavBar = ({score, topScore}) => (
-  <nav className="navbar">
-   <div className="scores">
-  <p>Score: {score}</p>
-  <p>Top Score: {topScore}</p>
-</div> // Ensure each score is in its own <p> element
-    //
-  </nav>
-)
+const NavBar = ({score, topScore}) => {
+  return (
+    <nav className="navbar">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png" // URL for the emoji game logo
+        alt="emoji logo" // Alt text as required
+        className="logo"
+      />
+      <div className="scores">
+        <p>Score: {score}</p>
+        <p>Top Score: {topScore}</p>
+      </div>
+    </nav>
+  )
+}
 
 export default NavBar
