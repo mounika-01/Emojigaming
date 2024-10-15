108b4466cddeb91b340f689dbfcd38483c0441c1
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-10-15 14:28:52 +0530

    Update index.js

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,15 +1,20 @@
-// src/components/EmojiCard/index.js
 import React from 'react'
 import './index.css'
 
-const EmojiCard = ({emoji, onClickEmoji}) => (
-  <button
-    type="button"
-    className="emoji-button"
-    onClick={() => onClickEmoji(emoji.id)}
-  >
-    <img src={emoji.emojiUrl} alt={emoji.emojiName} className="emoji-image" />
-  </button>
-)
+const EmojiCard = ({emoji, onEmojiClick}) => {
+  const {emojiName, emojiUrl} = emoji
+
+  const handleClick = () => {
+    onEmojiClick(emoji.id)
+  }
+
+  return (
+    <li className="emoji-item">
+      <button type="button" onClick={handleClick}>
+        <img src={emojiUrl} alt={emojiName} />
+      </button>
+    </li>
+  )
+}
 
 export default EmojiCard
